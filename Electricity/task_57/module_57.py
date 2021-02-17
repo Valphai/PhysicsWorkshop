92a05be8c18c78d6e3ec6c3f1c79d4afa9de0442
Valphai <Maneqinpl@gmail.com>
2021-02-17 11:40:02 +0100

    Changes to module_57

--- Electricity/task_57/module_57.py
+++ Electricity/task_57/module_57.py
@@ -8,11 +8,11 @@ R3 = kolumny["K2"]
 T = kolumny["K3"]
 
 def Eg():
-    k = 1.380651 * 1E-23
+    k = 8.617 * 1e-5
     a = 3347.546 # Got from calculation in R
     return k * a * 2
 
-if __name__ == "__main__":
-    print("R1: a: {}, u(x): {},\nR3: a: {}, u(x): {},\nEg: {}".format(un.a(R1,T),un.u(R1,T), 
-                                                                un.a(R3,T),un.u(R3,T),Eg()))
+# if __name__ == "__main__":
+print("R1: a: {}, u(x): {},\nR3: a: {}, u(x): {},\nEg: {}".format(un.a(R1,T),un.u(R1,T), 
+                                                            un.a(R3,T),un.u(R3,T),Eg()))
                                                                                         
